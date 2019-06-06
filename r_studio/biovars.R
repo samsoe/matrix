ea690100a9aa858435e8077d39322d60e9f94e31
Samsoe <esamsoe@gmail.com>
2019-06-06 10:44:39 -0600

    update comments

--- r_studio/biovars.R
+++ r_studio/biovars.R
@@ -1,7 +1,11 @@
 library("dismo")
 
-# read file
+# the biovars function in the dismo library requires average monthly temperature maximum, minimum and monthly accumulated precipitation values
+# the data source was generated with this notebook: http://bit.ly/2QQbCWf
+
 # data source download: http://bit.ly/2Z6hA8p
+# read file
+
 source = "~/Projects/matrix/output/temperature_precip-190604.csv"
 MyData <- read.csv(file = source, header=TRUE, sep=",")
 
